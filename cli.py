1d43e62983272e099478178244a7fc63a6db548f
ChristfriedBalizou <christfriedbalizou@gmail.com>
2017-12-14 10:52:20 +0100

    refactor: cli.py with PEP rules

--- cli.py
+++ cli.py
@@ -1,3 +1,7 @@
+'''
+cli.py
+'''
+
 from tools.modelize import Modelize
 from tools.template import Dotit
 
@@ -5,22 +9,27 @@ import pygraphviz as graph
 
 import argparse
 import sys
-import json
 import os
 
 
-def modelize(*args, **kwargs):
+def modelize(**kwargs):
+    '''
+    modelize introspect and extract data in json format
+    '''
     return Modelize(**kwargs)
 
 
-def draw(*args, **kwargs):
+def draw(filename, docs=[], relations=[], layout='dot'):
+    '''
+    Use pygraph to draw class diagram
+    '''
 
-    dot = Dotit(**kwargs)
+    dot = Dotit(docs=docs, relations=relations)
 
     libdG = graph.AGraph(dot.render())
-    libdG.layout(prog=options.layout)
+    libdG.layout(prog=layout)
 
-    output = os.path.expanduser(options.output)
+    output = os.path.expanduser(filename)
     directory = os.path.dirname(output)
 
     if directory == '':
@@ -34,62 +43,42 @@ def draw(*args, **kwargs):
     return output
 
 
-def run(options):
-
-    model = modelize(
-            directory=options.directory,
-            table_list=options.table_list,
-            draw_relations=options.relations,
-            relation_criterion=options.pattern)
-
-
-    if options.print_tables:
-        return model.tables()
-
-    return draw(docs=model.dot(), relations=model.dot_relations())
-
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-o', '--output',
-            default='diagram.png',
-            help='Output file path'
-            )
+                        default='diagram.png',
+                        help='Output file path')
 
     parser.add_argument('-r', '--relations',
-            default=False,
-            action='store_true',
-            help='Draw relations on graph if mentioned')
+                        default=False,
+                        action='store_true',
+                        help='Draw relations on graph if mentioned')
 
     parser.add_argument('--print-tables',
-            default=False,
-            action='store_true',
-            help='Print all tables in database scheme')
+                        default=False,
+                        action='store_true',
+                        help='Print all tables in database scheme')
 
     parser.add_argument('-t', '--table-list',
-            default=[],
-            nargs='*',
-            dest='table_list',
-            help='Table list to match'
-            )
+                        default=[],
+                        nargs='*',
+                        dest='table_list',
+                        help='Table list to match')
 
     parser.add_argument('-i', '--input-directory',
-            default=None,
-            dest='directory',
-            help='Tables scheme directory'
-            )
+                        default=None,
+                        dest='directory',
+                        help='Tables scheme directory')
 
     parser.add_argument('-p', '--pattern',
-            default='^REC',
-            help="Do not create relation with the follogin match",
-            )
+                        default='^REC',
+                        help="Do not create relation with the follogin match")
 
     parser.add_argument('-l', '--layout',
-            default='dot',
-            help="Program used by Graphiz to draw the graph",
-            )
+                        default='dot',
+                        help="Program used by Graphiz to draw the graph")
 
     options = parser.parse_args()
 
@@ -97,5 +86,14 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit(1)
 
+    model = modelize(table_list=options.table_list,
+                     draw_relations=options.relations,
+                     relation_criterion=options.pattern)
 
-    print run(options)
+    if options.print_tables:
+        print model.tables()
+
+    print  draw(docs=model.dot(),
+                relations=model.dot_relations(),
+                filename=options.directory,
+                layout=options.layout)
