25151a29661351325c1efc0e7a8af9f18ad3bd43
ChristfriedBalizou <christfriedbalizou@gmail.com>
2017-12-22 15:48:38 +0100

    admin: add alembic database schema migration utils

---
+++ alembic/versions/2f8d3f8b04ab_first_skeleton_of_relationship.py
@@ -0,0 +1,22 @@
+"""first skeleton of relationship
+
+Revision ID: 2f8d3f8b04ab
+Revises: None
+Create Date: 2017-12-22 15:45:34.101873
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2f8d3f8b04ab'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
